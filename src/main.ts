72ddc1b73408f2411b5ac435a7148ec860c616c5
thomrick <tricart@xebia.fr>
2017-05-22 10:48:23 +0200

    deploy core module exposition

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-class ApplicationStarter {
-  public static main(args: any) {
+export {Generator} from './common/interfaces/generator.interface';
+export {IPlayer} from './common/interfaces/player.interface';
+export {Question} from './common/interfaces/question.interface';
+export {Sender} from './common/interfaces/sender.interface';
+export {Session} from './common/interfaces/session.interface';
+export {Validator} from './common/interfaces/validator.interface';
 
-  }
-}
-ApplicationStarter.main(process.argv);
+export {AbstractGameSession} from './common/abstract-game.session';
+export {HttpQuestionSender} from './common/http-question.sender';
+export {Player} from './common/player';
+export {QuestionGenerator} from './common/question.generator';
